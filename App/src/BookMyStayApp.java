a9a62e24dd6520e54c6d086a64f1e88290248950
AkshatSharma-cs <AkshatSharma.chennai@gmail.com>
2026-03-18 09:15:07 +0530

    Application Entry & Welcome Message

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
 }
